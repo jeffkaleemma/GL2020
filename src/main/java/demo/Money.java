d89f1f1a0e9144126b91808170d8dc7e5f249c0f
jeff <jeffntumba@esisalama.org>
2020-11-20 12:52:31 +0200

    version initiale du code

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,4 +1,35 @@
 package demo;
 
 public class Money {
+    private int amount;
+    private String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+            "amount=" + amount +
+            ", currency='" + currency + '\'' +
+            '}';
+    }
 }
